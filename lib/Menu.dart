8e80a30601195f57b8062ef2823a89d240fe8ff6
Pdhenson <Pdhenson@mail.csuchico.edu>
2020-01-06 19:21:07 -0800

    Starting again after having lost some code.

---
+++ lib/Menu.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'main.dart';
+import 'ProfileScreen.dart';
+
+class FirstRoute extends StatelessWidget {
+  final UserDetails detailsUser;
+
+  FirstRoute({Key key, @required this.detailsUser}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Main Menu'),
+        ),
+        backgroundColor: Color.fromRGBO(23, 150, 50, 100),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network(
+                'https://freepngimg.com/download/phoenix_tattoos/8-2-phoenix-tattoos-png-picture.png',
+                fit: BoxFit.fill,
+                color: Color.fromRGBO(255, 255, 255, 0.6),
+                colorBlendMode: BlendMode.modulate),
+            ButtonBar(
+              buttonMinWidth: 400.00,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {
+                    // Navigate to the event creation screen.
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ThirdRoute(detailsUser)),
+                    );
+                    //createRecord(databaseReference, detailsUser);
+                    print("Moving to create record page.");
+                  },
+                  child: Text('Create Event'),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    // Navigate to the event cancellation screen.
+                  },
+                  child: Text('Cancel Event'),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    // Navigate to the event pending screen.
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SecondRoute()),
+                    );
+                  },
+                  child: Text('Check Pending Event'),
+                ),
+                RaisedButton(
+                  child: Text("Profile and Logout"),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              ProfileScreen(detailsUser: detailsUser)),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        )));
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return ListTile(
+      title: Row(
+        children: [
+          Expanded(
+            child: Text(
+              "Events:",
+              style: Theme.of(context).textTheme.headline,
+            ),
+          ),
+          Container(
+              decoration: const BoxDecoration(
+                color: Colors.blueGrey,
+              ),
+              padding: const EdgeInsets.all(1.0),
+              width: 300.00,
+              child: RaisedButton(
+                  child: Text(
+                    document.documentID,
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                  onPressed: () {
+                    // Navigate to the actual event screen.
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => FourthRoute(document.documentID, document)),
+                    );
+                  })),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Pending"),
+      ),
+      body: StreamBuilder(
+          stream: Firestore.instance.collection('events').snapshots(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) return const Text('Loading Data...');
+
+            return ListView.builder(
+                itemExtent: 80.0,
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot ds = snapshot.data.documents[index];
+                  return _buildListItem(context, ds);
+                });
+          }),
+    );
+  }
+}
+
+
+class FourthRoute extends StatelessWidget {
+  final String barName;
+  final DocumentSnapshot ds;
+  FourthRoute(this.barName, this.ds);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(barName)),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Who's In?"),
+                  Text(ds.data['userEmail']),
+                  Text(ds.data['eventName']),
+                  Text(ds.data['location']),
+                  Text(ds.data['time']),
+                ])));
+  }
+}
+
+class ThirdRoute extends StatelessWidget {
+  final eventName = TextEditingController();
+  final location = TextEditingController();
+  final time = TextEditingController();
+
+  final databaseReference = Firestore.instance;
+  final UserDetails detailsUser;
+
+  ThirdRoute(this.detailsUser);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sending out the call!"),
+      ),
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  controller: eventName,
+                  decoration:
+                  InputDecoration(labelText: 'What\'s the event called?'),
+                ),
+                TextFormField(
+                  controller: location,
+                  decoration: InputDecoration(
+                      labelText: 'Where\'s it going down?'),
+                ),
+                TextFormField(
+                  controller: time,
+                  decoration: InputDecoration(
+                      labelText: 'When\'s it happening?'),
+                ),
+                FloatingActionButton(onPressed: () {
+                  createRecord(
+                      databaseReference, detailsUser, eventName, location,
+                      time);
+                  print("the record has been created");
+                }),
+              ])),
+    );
+  }
+
+  Future createRecord(databaseReference, detailsUser, eventName, location,
+      time) async {
+    await databaseReference
+        .collection("events")
+        .document(eventName.text)
+        .setData({
+      'userEmail': detailsUser.userEmail,
+      'eventName': eventName.text,
+      'location': location.text,
+      'time': time.text,
+    });
+  }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'Menu.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'You In? Sign in.',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff9C58D2),
+      ),
+      home: GoogleSignApp(),
+    );
+  }
+}
+
+class GoogleSignApp extends StatefulWidget {
+  @override
+  _GoogleSignAppState createState() => _GoogleSignAppState();
+}
+
+class _GoogleSignAppState extends State<GoogleSignApp> {
+
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final GoogleSignIn _googlSignIn = new GoogleSignIn();
+
+  final db = Firestore.instance;
+
+  Future<FirebaseUser> _signIn(BuildContext context) async {
+
+    Scaffold.of(context).showSnackBar(new SnackBar(
+      content: new Text('Sign in'),
+    ));
+
+    final GoogleSignInAccount googleUser = await _googlSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+    );
+
+
+
+    var name = db.collection("users").toString();
+
+
+
+    final AuthResult authResult = await _firebaseAuth.signInWithCredential(credential);
+    final FirebaseUser userDetails = authResult.user;
+
+    ProviderDetails providerInfo = new ProviderDetails(userDetails.providerId);
+
+    List<ProviderDetails> providerData = new List<ProviderDetails>();
+    providerData.add(providerInfo);
+
+    UserDetails details = new UserDetails(
+      userDetails.providerId,
+      userDetails.displayName,
+      userDetails.photoUrl,
+      userDetails.email,
+      providerData,
+    );
+
+    createUser(db, details);
+    Navigator.push(
+      context,
+      new MaterialPageRoute(
+        builder: (context) => FirstRoute(detailsUser: details),//new ProfileScreen(detailsUser: details),
+      ),
+    );
+    return userDetails;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Builder(
+        builder: (context) => Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height:  MediaQuery.of(context).size.height,
+              child: Image.network(
+                  'https://cdn.clipart.email/040c32754ef22cf9829b20e6bc3ed0a0_anime-wallpaper-phone-32-images-on-genchiinfo_1080-1920.jpeg'
+                  ,fit: BoxFit.fill,
+                  color: Color.fromRGBO(255, 255, 255, 0.6),
+                  colorBlendMode: BlendMode.modulate
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height:10.0),
+                Container(
+                    width: 250.0,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(30.0)),
+                        color: Color(0xffffffff),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Icon(FontAwesomeIcons.google,color: Color(0xffCE107C),),
+                            SizedBox(width:10.0),
+                            Text(
+                              'Sign in with Google',
+                              style: TextStyle(color: Colors.black,fontSize: 18.0),
+                            ),
+                          ],),
+                        onPressed: () => _signIn(context)
+                            .then((FirebaseUser user) => print(user))
+                            .catchError((e) => print(e)),
+                      ),
+                    )
+                ),
+
+                              ],
+            ),
+          ],
+        ),),
+    );
+  }
+}
+
+
+
+class UserDetails {
+  final String providerDetails;
+  final String userName;
+  final String photoUrl;
+  final String userEmail;
+  final List<ProviderDetails> providerData;
+
+  UserDetails(this.providerDetails,this.userName, this.photoUrl,this.userEmail, this.providerData);
+}
+
+
+class ProviderDetails {
+  ProviderDetails(this.providerDetails);
+  final String providerDetails;
+}
+
+Future createUser(databaseReference, detailsUser) async {
+  await databaseReference
+      .collection("users")
+      .document(detailsUser.userName)
+      .setData({
+    'userName': detailsUser.userName,
+    'userEmail': detailsUser.userEmail,
+    'photo': detailsUser.photoUrl,
+    'loginTime': DateTime.now(),
+
+  });
+
+}
+
